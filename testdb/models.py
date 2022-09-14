e5c0bb44fcaef7bc18fbbf47d826aa49c8150675
Harsh <harshvyas9990@gmail.com>
2022-09-14 12:54:20 +0530

    Project v1

---
+++ testdb/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    class Meta:
+        app_label = 'testdb'
+        db_table='django\".\"teacher'
+        
+class Student(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    class Meta:
+        app_label = 'testdb'
+        db_table='custom\".\"student'
+        
+        
+class Studenttest(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    class Meta:
+        app_label = 'testdb'
+        db_table='users\".\"studenttest'
+        
+class Studentprod(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    class Meta:
+        app_label = 'testdb'
+        db_table='users\".\"studentprod'
+        
+class Teachertest(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    class Meta:
+        app_label = 'testdb'
+
+class Teacherprod(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
+    class Meta:
+        app_label = 'testdb'
---
+++ testpr/dbrouters/dbrouters.py
@@ -0,0 +1,142 @@
+from pickle import TRUE
+
+
+class DefaultDBRouter:
+
+    route_app_labels = { 'admin', 'auth', 'contenttypes',  'messages', 'staticfiles', 'testdb', 'sessions' }
+    testdefaultdb={'Teacher','Student'}
+
+    def db_for_read(self, model, **hints):
+        """
+        If the model is from app1, it suggests querying the app1 database. If the model is from app2, then app2
+        """
+        if (model._meta.app_label in self.route_app_labels and model.__class__.__name__ in self.testdefaultdb):
+            return 'default'
+        elif model._meta.app_label in self.route_app_labels and model._meta.app_label!='testdb':
+            return 'default'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        If the model is from app1, it suggests querying the app1 database. If the model is from app2, then app2
+        """
+        if (model._meta.app_label in self.route_app_labels and model.__class__.__name__ in self.testdefaultdb):
+            return 'default'
+        elif (model._meta.app_label in self.route_app_labels and model._meta.app_label!='testdb'):
+            return 'default'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the app1 or app2 apps is
+        involved.
+        """
+        if (
+            (obj1._meta.app_label in self.route_app_labels and obj1.__class__.__name__ in self.testdefaultdb) or
+            (obj2._meta.app_label in self.route_app_labels and obj2.__class__.__name__ in self.testdefaultdb)
+        ):
+            return True
+        elif (
+            (obj1._meta.app_label in self.route_app_labels and obj1._meta.app_label!='testdb') or
+            (obj2._meta.app_label in self.route_app_labels and obj2._meta.app_label!='testdb')
+        ):    
+            return True
+        else:
+            return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the app1 and app2 apps only appear in the
+        app1 or app2 database.
+        """
+        if app_label in self.route_app_labels and (model_name=="teacher" or model_name=="student"):
+            return db == 'default'
+        elif app_label in self.route_app_labels and app_label!='testdb':
+            return db == 'default'
+        return None
+
+class ProdDBRouter:
+
+    route_app_labels = {'testdb'}
+    proddb={'Teacherprod','Studentprod'}
+
+    def db_for_read(self, model, **hints):
+        """
+        If the model is from app1, it suggests querying the app1 database. If the model is from app2, then app2
+        """
+        if model._meta.app_label in self.route_app_labels and model.__class__.__name__ in self.proddb:
+            return 'proddb'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        If the model is from app1, it suggests querying the app1 database. If the model is from app2, then app2
+        """
+        if model._meta.app_label in self.route_app_labels and model.__class__.__name__ in self.proddb:
+            return 'proddb'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the app1 or app2 apps is
+        involved.
+        """
+        if (
+                (obj1._meta.app_label in self.route_app_labels and obj1.__class__.__name__ in self.proddb) or
+                (obj2._meta.app_label in self.route_app_labels and obj2.__class__.__name__ in self.proddb) 
+        ):
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the app1 and app2 apps only appear in the
+        app1 or app2 database.
+        """
+        if (model_name=="teacherprod" or model_name=="studentprod"):
+            if app_label in self.route_app_labels:
+                return db == 'proddb'
+        return None
+    
+class TestDBRouter:
+
+    route_app_labels = {'testdb'}
+    testdab={'Teachertest','Studenttest'}
+
+    def db_for_read(self, model, **hints):
+        """
+        If the model is from app1, it suggests querying the app1 database. If the model is from app2, then app2
+        """
+        if model._meta.app_label in self.route_app_labels and model.__class__.__name__ in self.testdab:
+            return 'testdbse'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        If the model is from app1, it suggests querying the app1 database. If the model is from app2, then app2
+        """
+        if model._meta.app_label in self.route_app_labels and model.__class__.__name__ in self.testdab:
+            return 'testdbse'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the app1 or app2 apps is
+        involved.
+        """
+        if (
+                (obj1._meta.app_label in self.route_app_labels and obj1.__class__.__name__ in self.testdab) or
+                (obj2._meta.app_label in self.route_app_labels and obj2.__class__.__name__ in self.testdab) 
+        ):
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the app1 and app2 apps only appear in the
+        app1 or app2 database.
+        """
+        if (model_name=="teachertest" or model_name=="studenttest"):
+            if app_label in self.route_app_labels:
+                return db == 'testdbse'
+        return None
